502f9ede11b9925855f89b220262050bf0c7bb50
Shravan Sheth <shravansheth@Mac.lan>
2025-05-22 19:25:41 -0700

    another small fix

--- packages/proto/src/project-list.ts
+++ packages/proto/src/project-list.ts
@@ -1,4 +1,4 @@
-import { html, LitElement } from "lit";
+import { html, css, LitElement } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import reset from "./styles/reset.css.ts"; 
 
