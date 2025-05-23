0f00bc6903582ce4d7994067a985d0a4608207b9
Shravan Sheth <shravansheth@Mac.lan>
2025-05-22 19:23:09 -0700

    small fix

--- packages/proto/src/project-list.ts
+++ packages/proto/src/project-list.ts
@@ -1,49 +1,69 @@
 import { html, LitElement } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
+import reset from "./styles/reset.css.ts"; 
 
 interface Project {
-  title: string;
-  submitter: string;
-  tools: string;
-  description: string;
-  href: string;
+    title: string;
+    submitter: string;
+    tools: string;
+    description: string;
+    href: string;
 }
 
 @customElement("project-list")
 export class ProjectListElement extends LitElement {
-  @property()
-  src?: string;
+    @property()
+    src?: string;
 
-  @state()
-  projects: Project[] = [];
+    @state()
+    projects: Project[] = [];
 
-  connectedCallback() {
-    super.connectedCallback();
-    if (this.src) this.hydrate(this.src);
-  }
+    connectedCallback() {
+        super.connectedCallback();
+        if (this.src) this.hydrate(this.src);
+    }
 
-  hydrate(src: string) {
-    fetch(src)
-      .then(res => res.json())
-      .then((json: object) => {
-        if (Array.isArray(json)) {
-          this.projects = json as Project[];
-        }
-      });
-  }
+    hydrate(src: string) {
+        fetch(src)
+            .then((res) => res.json())
+            .then((json: object) => {
+                if (Array.isArray(json)) {
+                    this.projects = json as Project[];
+                }
+            });
+    }
 
-  renderProject(p: Project) {
-    return html`
-      <wood-project
-        title="${p.title}"
-        submitter="${p.submitter}"
-        description="${p.description}"
-        href="${p.href}"
-      ></wood-project>
-    `;
-  }
+    renderProject(p: Project) {
+        return html`
+            <wood-project
+                title="${p.title}"
+                submitter="${p.submitter}"
+                description="${p.description}"
+                href="${p.href}"
+            ></wood-project>
+        `;
+    }
 
-  render() {
-    return html` ${this.projects.map(this.renderProject)} `;
-  }
+    render() {
+        return html`
+            <div class="project-list">
+                ${this.projects.map(this.renderProject)}
+            </div>
+        `;
+    }
+
+    static styles = [
+        reset.styles,
+        css`
+            .project-list {
+                display: flex;
+                flex-wrap: wrap;
+                gap: 1rem;
+            }
+
+            wood-project {
+                flex: 1 1 300px;
+            }
+        `,
+    ];
 }
