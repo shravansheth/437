cc682b43faab7a3b6a5f0a51bcc3dc8dede8faa4
Shravan Sheth <shravansheth@Mac.lan>
2025-06-05 21:35:41 -0700

    s

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -10,7 +10,7 @@ import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const staticDir = process.env.STATIC || "public";
+const staticDir = process.env.STATIC || "../app/dist";
 
 app.use(express.static(staticDir));
 app.use(express.json());
