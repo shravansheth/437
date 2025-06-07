6a3062a26951d19e8646bf4aecfa5abbf600a9f3
Shravan Sheth <shravansheth@Mac.lan>
2025-06-06 19:03:37 -0700

    igotit

--- packages/server/src/routes/projects.ts
+++ packages/server/src/routes/projects.ts
@@ -11,6 +11,16 @@ router.get("/", (_req: Request, res: Response) => {
 });
 
 // Get one project by slug
+// router.get("/:slug", (req: Request, res: Response) => {
+//   const { slug } = req.params;
+//   Projects.get(slug)
+//     .then((project: Project | null) => {
+//       if (project) res.json(project);
+//       else res.status(404).end();
+//     })
+//     .catch((err) => res.status(500).send(err));
+// });
+
 router.get("/:slug", async (req, res) => {
   const { slug } = req.params;
 
